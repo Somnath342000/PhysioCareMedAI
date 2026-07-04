542971c28da869eaef0b08a2b01a7df3cd2f4fb6
Somnath Banerjee <somnathbanerjee342000@gmail.com>
2026-07-04 07:20:30 +0530

    Medicine_Info.py
    Updated code

--- pages/2_Medicine_Info.py
+++ pages/2_Medicine_Info.py
@@ -1,1 +1,29 @@
-Medicine
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Medicine Information")
+
+st.title("💊 Medicine Information")
+
+df = pd.read_csv("data/medicines.csv")
+
+medicine = st.selectbox(
+    "Select Medicine",
+    df["Medicine"]
+)
+
+row = df[df["Medicine"] == medicine].iloc[0]
+
+st.subheader("Generic Name")
+st.write(row["Generic"])
+
+st.subheader("Uses")
+st.write(row["Uses"])
+
+st.subheader("Side Effects")
+st.write(row["Side Effects"])
+
+st.subheader("Warning")
+st.write(row["Warning"])
+
+st.warning("⚠️ Do not take any medicine without consulting a qualified healthcare professional.")
